2a1074a7068e040b3f015da718679970a3c6bc5e
sj84593n <58002474+sj84593n@users.noreply.github.com>
2020-04-19 12:14:41 -0400

    Delete Insert Querry.sql

--- Insert Querry.sql
+++
@@ -1,7 +0,0 @@
-insert into Author values("sj84593n@pace.edu","shashank", "Jadhav");
-insert into Paper values(1,"sj84593n@pace.edu","Data Science", "Data Science Demo", "FinalDBMS.sql");
-insert into Reviewer values("rn84593n@pace.edu","Rushikesh", "Good", "Nage", "Good","1021222109","Pace University",1);
-insert into Review values("rn84593n@pace.edu", "changes", 1, 10, 1, 10, 10, 10, 10);
-insert into Topic values("rn84593n@pace.edu", 1, "DataScience");
-
-
