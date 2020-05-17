60fe9ce7147dd43be5859fe77012e09e03288622
sj84593n <58002474+sj84593n@users.noreply.github.com>
2020-05-16 22:16:58 -0400

    Delete SELECT QUERY.sql

--- SELECT QUERY.sql
+++
@@ -1,6 +0,0 @@
-select * FROM Author;
-select * FROM Paper;
-select * FROM Reviewer;
-select * FROM Review;
-select * FROM Topic;
-select * FROM Paper left join Author on Paper.EmailAddr = Author.EmailAddr;
